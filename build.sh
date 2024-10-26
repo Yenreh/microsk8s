130e8b6ad55787499493edb90bbe8108a3cb4680
yenreh <qherney@gmail.com>
2024-10-26 12:44:33 -0500

    General Update

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker build -t yenreh/apigw ./apigw
 docker build -t yenreh/authservice ./authservice
-docker build -t yenreh/servicioa ./servicioa
-docker build -t yenreh/serviciob ./serviciob
-docker build -t yenreh/servicioc ./servicioc
+docker build -t yenreh/servicio-a ./servicioa
+docker build -t yenreh/servicio-b ./serviciob
+docker build -t yenreh/servicio-c ./servicioc
--- upload.sh
+++ upload.sh
@@ -2,7 +2,7 @@
 
 docker push yenreh/apigw
 docker push yenreh/authservice
-docker push yenreh/servicioa
-docker push yenreh/serviciob
-docker push yenreh/servicioc
+docker push yenreh/servicio-a
+docker push yenreh/servicio-b
+docker push yenreh/servicio-c
 
