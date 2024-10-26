7a0ba58fdf877bebaad190d2e9454f51e05b959a
yenreh <qherney@gmail.com>
2024-10-26 11:16:55 -0500

    General Update

--- delete.sh
+++ delete.sh
@@ -4,10 +4,10 @@ kubectl delete pod servicio-a-pod
 kubectl delete pod servicio-b-pod
 kubectl delete pod apigw-pod
 kubectl delete pod authservice-pod
-#kubectl delete pod servicio-orquesta-pod
+kubectl delete pod redis-pod
 
 kubectl delete service servicio-a-service
 kubectl delete service servicio-b-service
 kubectl delete service apigw-service
 kubectl delete service authservice-service
-#kubectl delete service servicio-orquesta-service
+kubectl delete service redis-service
