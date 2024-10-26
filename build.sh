02e65b8a1e8025c20a58aef4cf3143abf3395b67
yenreh <qherney@gmail.com>
2024-10-26 13:05:25 -0500

    General Update

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+docker rmi yenreh/apigw
+docker rmi yenreh/authservice
+docker rmi yenreh/servicio-a
+docker rmi yenreh/servicio-b
+docker rmi yenreh/servicio-c
+
 docker build -t yenreh/apigw ./apigw
 docker build -t yenreh/authservice ./authservice
 docker build -t yenreh/servicio-a ./servicioa
