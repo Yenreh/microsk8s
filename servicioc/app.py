f482b296145b8c380be97be5b3e7d6a08953440a
yenreh <qherney@gmail.com>
2024-10-26 11:30:09 -0500

    General Update

---
+++ servicioc/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/servicio-c', methods=['GET'])
+def service():
+    resultado = "Yenreh Service Test"
+    return jsonify({"message": resultado})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3001)
