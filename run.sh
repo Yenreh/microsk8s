0e1404811b9cb3d635c5f965404f40684ceab654
yenreh <qherney@gmail.com>
2024-10-26 11:33:36 -0500

    General Update

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@ kubectl apply -f authservice/redis.yml
 kubectl apply -f authservice/authservice.yml
 kubectl apply -f servicioa/servicioa.yml
 kubectl apply -f serviciob/serviciob.yml
+kubectl apply -f servicioc/servicioc.yml
 kubectl apply -f apigw/apigw.yml
 
 kubectl port-forward svc/apigw-service 4000:80
